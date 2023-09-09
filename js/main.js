0533ce598b741fddeda2ea86a645da38cae4e4b7
sofi <sofimusha@gmail.com>
2023-09-09 11:23:38 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
